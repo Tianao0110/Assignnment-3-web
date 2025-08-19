4d549ee6a605fbdddc6c577d8e0ec4d7a42271a4
Tianao0110 <edan0110@gmail.com>
2025-08-19 00:20:26 -0600

    contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,15 +1,16 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
-document
-  .getElementById("submit-button")
-  .addEventListener("click", function (event) {
+document.getElementById('submit-button').addEventListener('click', function (event) {
     event.preventDefault();
-    document.querySelector(".contact").innerHTML =
-      "<p style='font-size: 24px'>Thank you for your message</p>";
-  });
+    document.getElementById('contact-page').innerHTML = '';
+    const thankYouMessage = document.createElement('p');
+    thankYouMessage.textContent = 'Thank you for your message';
+    thankYouMessage.classList.add('submit-message');
+    document.getElementById('contact-page').appendChild(thankYouMessage);
+    document.getElementById('contact-page').classList.add('message-sent');
+});
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
-
-
+document.querySelectorAll(".blue-hover").forEach(function (el) {
   el.addEventListener("mouseover", function (event) {
     event.target.style.color = "white";
     event.target.style.textDecoration = "underline";
